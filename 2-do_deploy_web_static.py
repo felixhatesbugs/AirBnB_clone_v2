0002b8d008ef5787fa1d03f41664a65b87725901
6felix <felix.okoronkwo.1@gmail.com>
2023-05-13 00:59:23 +0100

    deploys web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,6 +23,16 @@ def do_deploy(archive_path):
     # Uploads archive to webserver
     if put(archive_path, '/tmp/{}'.format(file)).failed is True:
         return False
+    
+    # deletes archive destination is exists
+    if run("rm -rf /data/web_static/current/{}".
+           format(name)).failed is True:
+        return False
+    
+    # creates a new one
+    if run("mkdir -p /data/web_static/current/{}".
+           format(name)).failed is True:
+        return False
 
     # Uncompress the archive to the folder
     if run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'
@@ -30,7 +40,7 @@ def do_deploy(archive_path):
         return False
 
     # Delete the archive from the web server
-    if run('rm -rf /tmp/{}'.format(file)).failed is True:
+    if run('rm /tmp/{}'.format(file)).failed is True:
         return False
 
     # Delete the symbolic link current
@@ -38,8 +48,8 @@ def do_deploy(archive_path):
         return False
 
     # Create a new the symbolic link
-    if run('ln -sf /data/web_static/releases/{} \
-            /data/web_static/current').failed is True:
+    if run('ln -s /data/web_static/releases/{} /data/web_static/current'.
+           format(name)).failed is True:
         return False
 
     # Success
