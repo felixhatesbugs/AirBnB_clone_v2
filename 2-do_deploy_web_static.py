34d32fcbdb497568a10572233d36ab1a4e2631ff
6felix <felix.okoronkwo.1@gmail.com>
2023-05-13 00:47:49 +0100

    deploys web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,9 +8,10 @@ from fabric.api import run, put, env
 
 env.hosts = ["18.204.9.215", "54.144.45.169"]
 
+
 def do_deploy(archive_path):
     """deploys archive to server"""
-    
+
     # Checks is the file exists
     if os.path.isfile(archive_path) is False:
         return False
@@ -18,16 +19,16 @@ def do_deploy(archive_path):
     # Finds the name of the file
     file = archive_path.split('/')[-1]
     name = file.split('.')[0]
-    
+
     # Uploads archive to webserver
     if put(archive_path, '/tmp/{}'.format(file)).failed is True:
         return False
-    
+
     # Uncompress the archive to the folder
     if run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'
            .format(file, name)).failed is True:
         return False
-    
+
     # Delete the archive from the web server
     if run('rm -rf /tmp/{}'.format(file)).failed is True:
         return False
@@ -37,9 +38,9 @@ def do_deploy(archive_path):
         return False
 
     # Create a new the symbolic link
-    if run('ln -sf /data/web_static/releases/{} /data/web_static/current')
-           .failed is True:
+    if run('ln -sf /data/web_static/releases/{} \
+            /data/web_static/current').failed is True:
         return False
-    
+
     # Success
     return True
