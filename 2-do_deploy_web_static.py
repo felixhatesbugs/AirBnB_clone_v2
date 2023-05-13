817fb7f28ce810f77f3413fd16f427afd851c75b
6felix <felix.okoronkwo.1@gmail.com>
2023-05-13 08:40:30 +0100

    deploys web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,17 +25,17 @@ def do_deploy(archive_path):
         return False
     
     # deletes archive destination is exists
-    if run("rm -rf /data/web_static/current/{}".
+    if run("rm -rf /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
-    
+
     # creates a new one
-    if run("mkdir -p /data/web_static/current/{}".
+    if run("mkdir -p /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
 
     # Uncompress the archive to the folder
-    if run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'
+    if run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
            .format(file, name)).failed is True:
         return False
 
@@ -43,12 +43,16 @@ def do_deploy(archive_path):
     if run('rm /tmp/{}'.format(file)).failed is True:
         return False
 
+    if run("mv /data/web_static/releases/{}/web_static/* \
+           /data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+
     # Delete the symbolic link current
     if run('rm -rf /data/web_static/current').failed is True:
         return False
 
-    # Create a new the symbolic link
-    if run('ln -s /data/web_static/releases/{} /data/web_static/current'.
+    # Create a new symbolic link
+    if run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.
            format(name)).failed is True:
         return False
 
