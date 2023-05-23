6a14d1ecd9687cba832c216425b18f077fe4b738
6felix <felix.okoronkwo.1@gmail.com>
2023-05-23 10:04:22 +0100

    adds hbnb route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""adds /hbnb route to the server"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def return_hello():
+    """
+    returns hello world
+    when root is queried
+    """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def return_hbnb():
+    """
+    returns page for
+    /hbnb route query
+    """
+    return 'HBNB'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
