51b7f39c8feb2cfc0a266f921e8e7c44fcd34ab5
6felix <felix.okoronkwo.1@gmail.com>
2023-05-25 18:02:25 +0100

    added /cities_by_states routes in server

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # HBNB
 
-This is the console /command interpreter for the Holberton Airbnb clone project. The console can be used to store objects in and retrieve objects using either a MySQL database, or a JSON file.
+This is the console /command interpreter for the ALX/Holberton Airbnb clone project. The console can be used to store objects in and retrieve objects using either a MySQL database, or a JSON file.
 
 ### Supported classes:
 * BaseModel
@@ -23,38 +23,3 @@ To start the console using JSON file storage, navigate to the project folder and
 
 To start the console using MySQL database as storage method, navigate to the project folder and enter:
 `HBNB_MYSQL_USER=<username> HBNB_MYSQL_PWD=<password HBNB_MYSQL_HOST=<server hostname> HBNB_MYSQL_DB=<database name> HBNB_TYPE_STORAGE=db ./console.py`
-
-#### Create
-`create <class name> [<attr name>="<attr value>" ...]`
-Ex:
-`create BaseModel`
-`create State name="California"`
-`create State name="New_York"`
-
-#### Show
-`show <class name> <object id>`
-Ex:
-`show User my_id`
-
-#### Destroy
-`destroy <class name> <object id>`
-Ex:
-`destroy Place my_place_id`
-
-#### All
-`all` or `all <class name>`
-Ex:
-`all` or `all State`
-
-#### Quit
-`quit` or `EOF`
-
-#### Help
-`help` or `help <command>`
-Ex:
-`help` or `help quit`
-
-Additionally, the console supports `<class name>.<command>(<parameters>)` syntax.
-Ex:
-`City.show(my_city_id)`
-
---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+"""starts a Flask web application
+"""
+
+from models import storage
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def return_hello():
+    """
+    returns hello world
+    when root is queried
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def return_hbnb():
+    """
+    returns page for
+    /hbnb route query
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def return_c(text):
+    """
+    returns : “C ” followed by the value of the text
+    syntax  : /c/<text>
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def return_python(text="is_cool"):
+    """
+    returns : “Python ”, followed by the value of the text"
+    syntax  : /python/(<text>)
+    """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def return_n(n):
+    return "{:d} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def return_n_template(n):
+    """
+    returns rendered html templates
+    for number_template query
+    """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def return_odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+@app.teardown_appcontext
+def tear_session(self):
+    """removes current SQLAlchemy Session
+    after each session
+    """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def return_states():
+    """returns rendered html templates
+    of states in database
+    """
+    states = [v for v in storage.all("State").values()]
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def return_state_cities():
+    """returns rendered html templates
+    of cities by state in database
+    """
+    states = [v for v in storage.all("State").values()]
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
